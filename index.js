fb72b6cd6c87810e3dbf3b651701f6f67ec4f823
Daniel Silva <dansilvac254@gmail.com>
2025-05-01 03:17:50 -0300

    atualização de estilo(tamanho das fontes)

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const dayjs = require('./node_modules/dayjs');
 import utc from './node_modules/dayjs/plugin/utc';
-import timezone from './node_modules/dayjs/plugin/timezone';
 document.addEventListener('DOMContentLoaded', buscarHorario());
 
 function buscarHorario() {
     dayjs.extend(utc)
-    dayjs.extend(timezone)
 
     const hora = dayjs().format('HH:mm')
     const dia = dayjs().format('DD, MMMM YYYY');
     const horario = document.getElementById('horario');
     const horas = document.createElement('h1');
     const datas = document.createElement('h2');
+
+    horas.setAttribute('id', 'hora');
+    datas.setAttribute('id', 'dia');
 
     horas.textContent = hora;
     datas.textContent = dia;
